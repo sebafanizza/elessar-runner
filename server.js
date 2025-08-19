068cc51e305de86f81148cde7af53c078f396567
sebafanizza <seba.fanizza@gmail.com>
2025-08-19 12:13:56 +0200

    Update server.js

--- server.js
+++ server.js
@@ -3,12 +3,7 @@ const bodyParser = require('body-parser');
 
 const app = express();
 app.use(bodyParser.json());
-
 app.get('/', (req, res) => res.send('Elessar runner ok'));
 
 const PORT = process.env.PORT || 10000;
 app.listen(PORT, () => console.log('Runner up on ' + PORT));
-
-
-
-app.listen(process.env.PORT || 8080, ()=>console.log('Runner up'));
